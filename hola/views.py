af8745eb5bc06349eff9f9cf9eb17119282baa7d
Alejandro Alvarez <aleal76@hotmail.com>
2021-02-08 09:07:10 -0300

    solo ajuste texto

--- hola/views.py
+++ hola/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hola desde Django proyect app hola newsitefdsfdsf")
+    return HttpResponse("Hola desde Django proyect app hola")
